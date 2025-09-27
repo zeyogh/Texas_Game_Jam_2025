fc95a8b7805b117cae0d5a3fa171c5f7faaa3ec3
zeyogh <zeynep.ogh@gmail.com>
2025-09-27 10:45:54 -0500

    navigate through entire 11

--- out_of_light/scripts/room.gd
+++ out_of_light/scripts/room.gd
@@ -2,10 +2,9 @@ extends Control
 
 @export_file("*.tscn") var left_path: String
 @export_file("*.tscn") var right_path: String
-@export_file("*.png", "*.jpg", "*.jpeg") var ceiling_path: String
-@export_file("*.png", "*.jpg", "*.jpeg") var opposite_path: String
 
-@onready var front_texture: Texture2D = $TextureRect.texture
+@onready var left_visible: bool = $LeftButton.visible
+@onready var right_visible: bool = $RightButton.visible
 
 func _on_left_button_pressed() -> void:
 	get_tree().change_scene_to_file(left_path)
@@ -13,23 +12,20 @@ func _on_left_button_pressed() -> void:
 func _on_right_button_pressed() -> void:
 	get_tree().change_scene_to_file(right_path)
 
-func _on_ceiling_button_pressed() -> void:
-	var tex := load(ceiling_path) as Texture2D
-	$TextureRect.texture = tex
-	_set_visibility(false, false, false, false, true)
-
 func _on_opposite_button_pressed() -> void:
-	var tex := load(opposite_path) as Texture2D
-	$TextureRect.texture = tex
-	_set_visibility(false, false, true, false, true)
+	$FrontTexture.visible = false
+	$OppositeTexture.visible = true
+	_set_visibility(false, false, false, true)
 
 func _on_front_button_pressed() -> void:
-	$TextureRect.texture = front_texture
-	_set_visibility(true, true, true, true, false)
+	$FrontTexture.visible = true
+	$OppositeTexture.visible = false
+	_set_visibility(true, true, true, false)
 
-func _set_visibility(left: bool, right: bool, ceiling: bool, opposite: bool, front: bool) -> void:
-	$LeftButton.visible = left
-	$RightButton.visible = right
-	$CeilingButton.visible = ceiling
+func _set_visibility(left: bool, right: bool, opposite: bool, front: bool) -> void:
+	if left_visible:
+		$LeftButton.visible = left
+	if right_visible:
+		$RightButton.visible = right
 	$OppositeButton.visible = opposite
 	$FrontButton.visible = front
