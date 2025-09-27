3cc66639dfa64105c998f34db05f6f956fa90db1
zeyogh <zeynep.ogh@gmail.com>
2025-09-27 17:01:31 -0500

    post it notes and controllers

--- out_of_light/scripts/post_it_controller.gd
+++ out_of_light/scripts/post_it_controller.gd
@@ -5,9 +5,10 @@ var json_path = "res://json/dialogue.json"
 var dialogue: Array = []
 
 func _ready() -> void:
-	load_json_file()
+	_load_json_file()
+	pass
 	
-func load_json_file():
+func _load_json_file():
 	var file = FileAccess.open(json_path, FileAccess.READ)
 	assert(file.file_exists(json_path), "File path does not exist")
 	var json = file.get_as_text()
@@ -15,13 +16,19 @@ func load_json_file():
 	json_object.parse(json)
 	dialogue = json_object.data
 	
-	for element in dialogue:
-		var room = element["room"]
-		var progress = element["progress"]
-		var dialogue_array = element["dialogue_array"]
-		
-		print("Room:", room)
-		print("Progress:", progress)
-		print("Dialogue:", dialogue_array["p1"])
-		print("------")
+	#for element in dialogue:
+		#var room = element["room"]
+		#var progress = element["progress"]
+		#var dialogue_array = element["dialogue_array"]
+		#
+		#print("Room:", room)
+		#print("Progress:", progress)
+		#print("Dialogue:", dialogue_array["p1"])
+		#print("------")
 	
+func _call_post_it(room: String, progress: String):
+	for element in dialogue:
+		if element["room"] == room and element["progress"] == progress:
+			var dialogue_array = element["dialogue_array"]
+			var dialogue = dialogue_array["p1"]
+			return dialogue
