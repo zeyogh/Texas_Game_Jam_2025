aa3cb8a27d1c784dbf95788bff1756ce4871f9fc
zeyogh <zeynep.ogh@gmail.com>
2025-09-27 18:49:48 -0500

    intro cutscene

--- out_of_light/scripts/player_controller.gd
+++ out_of_light/scripts/player_controller.gd
@@ -1,3 +1,13 @@
 extends Node2D
 
 var current_room := RoomsEnum.Rooms.BEDROOM
+
+var has_matchbox := false
+
+var has_batteries := false
+
+var has_glasses := false
+
+var has_razor := false
+
+var has_key := false
