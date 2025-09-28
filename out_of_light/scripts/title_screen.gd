8c711998d89cedfaf5e3df3b5d39caa35c39d633
zeyogh <zeynep.ogh@gmail.com>
2025-09-28 17:49:48 -0500

    post it changes

--- out_of_light/scripts/title_screen.gd
+++ out_of_light/scripts/title_screen.gd
@@ -1,4 +1,4 @@
 extends Node
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/intro_cutscene.tscn")
+	get_tree().change_scene_to_file("res://scenes/description_scene.tscn")
