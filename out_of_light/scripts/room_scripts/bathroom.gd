7d7bea0d2231f9ddf688769ac01de37cddc91fc1
zeyogh <zeynep.ogh@gmail.com>
2025-09-28 09:53:48 -0500

    new intro cutscene and sound effects

--- out_of_light/scripts/room_scripts/bathroom.gd
+++ out_of_light/scripts/room_scripts/bathroom.gd
@@ -2,3 +2,6 @@ extends Control
 
 func _on_cabinet_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/rooms/bathroom_cabinet.tscn")
+
+func _on_mirror_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/rooms/bathroom_mirror.tscn")
