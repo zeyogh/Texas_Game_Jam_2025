b1b9396dbfd88c9149faec538dc1d8a583bd8663
zeyogh <zeynep.ogh@gmail.com>
2025-09-28 18:44:43 -0500

    jumpscare sound

--- out_of_light/scenes/rooms/bathroom_mirror.gd
+++ out_of_light/scenes/rooms/bathroom_mirror.gd
@@ -5,5 +5,6 @@ static var can_play_jumpscare := true
 func _ready() -> void:
 	if PlayerController.has_razor and can_play_jumpscare:
 		$MirrorButton/Person.texture = load("res://pictures/reflection_2.png")
+		$JumpscareSound.play()
 		can_play_jumpscare = false
 		
