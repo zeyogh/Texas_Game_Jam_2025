68f98c23bc1e9dbb566325b578ca20734c021231
zeyogh <zeynep.ogh@gmail.com>
2025-09-28 11:43:32 -0500

    stairs blocked off

--- out_of_light/scenes/rooms/stairs_bottom.gd
+++ out_of_light/scenes/rooms/stairs_bottom.gd
@@ -1,5 +1,8 @@
 extends Control
 
+func _ready() -> void:
+	if ProgressController.current_stage != ProgressEnum.Progress.STAGE_4 and ProgressController.current_stage != ProgressEnum.Progress.STAGE_5 and ProgressController.current_stage != ProgressEnum.Progress.OUTRO:
+		$RoomGeneric/RightButton.visible = false
 
 func _on_closet_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/rooms/closet.tscn")
