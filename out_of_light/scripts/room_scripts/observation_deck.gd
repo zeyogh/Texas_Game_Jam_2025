4cef3e5fdcd0a987d5881525f15c3aea3367b344
zeyogh <zeynep.ogh@gmail.com>
2025-09-28 18:30:30 -0500

    signals postit

--- out_of_light/scripts/room_scripts/observation_deck.gd
+++ out_of_light/scripts/room_scripts/observation_deck.gd
@@ -8,11 +8,13 @@ func _on_opposite_button_pressed() -> void:
 	if !PlayerController.has_key:
 		$KeyButton.visible = false
 		$LightButton.visible = false
+	$SignalsPostIt.visible = true
 
 func _on_front_button_pressed() -> void:
 	if !PlayerController.has_key:
 		$KeyButton.visible = true
 		$LightButton.visible = true
+	$SignalsPostIt.visible = false
 
 func _on_key_button_pressed() -> void:
 	PlayerController.has_key = true
