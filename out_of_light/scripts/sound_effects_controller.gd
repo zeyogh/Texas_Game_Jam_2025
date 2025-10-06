5a766437f28db2187f563b757a20f4f6f8cb64ff
tnanc <tnance610@gmail.com>
2025-10-05 20:50:51 -0500

    Lightning !

--- out_of_light/scripts/sound_effects_controller.gd
+++ out_of_light/scripts/sound_effects_controller.gd
@@ -1,4 +1,22 @@
 extends Node2D
 
+const dark_color : Color = Color(.1,.1,.1)
+
 func _on_timer_timeout() -> void:
 	$ThunderSounds.play()
+	_do_lightning()
+
+func _do_lightning() -> void:
+	var darkness : CanvasModulate = $CanvasModulate
+	await get_tree().create_timer(0.35).timeout
+	darkness.color = Color.WHITE
+	await get_tree().create_timer(0.15).timeout
+	darkness.color = dark_color
+	await get_tree().create_timer(0.75).timeout
+	darkness.color = Color.WHITE
+	await get_tree().create_timer(0.1).timeout
+	darkness.color = dark_color
+	await get_tree().create_timer(0.15).timeout
+	darkness.color = Color.WHITE
+	await get_tree().create_timer(0.2).timeout
+	darkness.color = dark_color
